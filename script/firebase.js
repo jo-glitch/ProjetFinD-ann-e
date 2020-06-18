ff8da4c32706025d9abb80bb38f62685dea5c0e2
Joan F <joanfoubertetude@gmail.com>
2020-06-18 18:14:59 +0200

    page playlist done

---
+++ script/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDS5_a2NaZkD2EJrc_8T9jLxl_wW3jVwMs",
+    authDomain: "projetcours-c393f.firebaseapp.com",
+    databaseURL: "https://projetcours-c393f.firebaseio.com",
+    projectId: "projetcours-c393f",
+    storageBucket: "projetcours-c393f.appspot.com",
+    messagingSenderId: "244990817381",
+    appId: "1:244990817381:web:40deb275741178208fc28c",
+    measurementId: "G-22XNPMH0Z9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
